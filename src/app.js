1cda91e28b61632693460d00121520fe04228dd7
Binner <lucian.binner@hotmail.com>
2021-07-02 12:51:24 -0300

    tdd authentication

--- src/app.js
+++ src/app.js
@@ -2,8 +2,11 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const JWTSecret = "dcfghfghj4536453*((&P?H02543fghbf";
 
 const user = require("./models/User");
+const { json } = require("express");
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -22,16 +25,41 @@ app.get("/", (req, res) => {
     return res.status(200).json({ success: true });
 });
 
+app.post("/auth", async (req, res) => {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ "email": email });
+
+
+    if (user === null) {
+        return res.status(403).json({ errors: { email: "E-mail não cadastrado!" } });
+    }
+
+    const isPasswordRight = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordRight) {
+        return res.status(403).json({ errors: { password: "Senha inválida!" } })
+    }
+
+    jwt.sign({ email, name: user.name, id: user._id }, JWTSecret, { expiresIn: '48h' }, (err, token) => {
+        if (err) {
+            return res.status(500).json(err);
+        } else {
+            return res.status(200).json({ token });
+        }
+    });
+});
+
 app.post("/user", async (req, res) => {
-    
+
     if (req.body.name === "" || req.body.email === "" || req.body.password === "") {
         return res.sendStatus(400);
     }
 
     try {
-        const user = await User.findOne({"email": req.body.email});
-        if(user !== null) {
-            return res.status(400).json({error: "E-mail já cadastrado"})
+        const user = await User.findOne({ "email": req.body.email });
+        if (user !== null) {
+            return res.status(400).json({ error: "E-mail já cadastrado" })
         }
 
         const password = req.body.password;
@@ -48,6 +76,15 @@ app.post("/user", async (req, res) => {
         return res.json({ email: req.body.email })
     } catch (error) {
         return res.status(500).json({ error })
+    }
+});
+
+app.delete("/user/:email", async (req, res) => {
+    try {
+        await User.deleteOne({ "email": req.params.email })
+        return res.status(200).json({ message: "Usuário deletado com sucesso!" });
+    } catch (error) {
+        return res.status(500).json({ error });
     }
 });
 
--- test/user.test.js
+++ test/user.test.js
@@ -2,11 +2,36 @@ const app = require("../src/app");
 const supertest = require('supertest');
 const request = supertest(app);
 
+const mainUser = {
+    name: "Lucian Binner",
+    email: "lucian@email.com",
+    password: "123456",
+};
+
+beforeAll(() => {
+    return request
+        .post("/user")
+        .send(mainUser)
+        .then(res => { })
+        .catch(err => {
+            console.log(err);
+        });
+});
+
+afterAll(() => {
+    return request
+        .delete(`/user/${mainUser.email}`)
+        .then(res => { })
+        .catch(err => {
+            console.log(err);
+        });
+});
+
 describe("Cadastro do usuário", () => {
     test("Deve cadastrar um usuário com sucesso", () => {
         const time = Date.now();
         const email = `${time}@email.com`;
-        const user = {name: "Lucian", email, password: "123456"};
+        const user = { name: "Lucian", email, password: "123456" };
 
         return request.post("/user")
             .send(user)
@@ -20,7 +45,7 @@ describe("Cadastro do usuário", () => {
     });
 
     test("Deve impedir que um usuário se cadastre com os dados vazios", () => {
-        const user = {name: "", email: "", password: ""};
+        const user = { name: "", email: "", password: "" };
 
         return request.post("/user")
             .send(user)
@@ -35,7 +60,7 @@ describe("Cadastro do usuário", () => {
     test("Deve impedir que um usuário se cadastre com um e-mail repetido", () => {
         const time = Date.now();
         const email = `${time}@email.com`;
-        const user = {name: "Binner", email, password: "123456"};
+        const user = { name: "Binner", email, password: "123456" };
 
         return request.post("/user")
             .send(user)
@@ -44,13 +69,55 @@ describe("Cadastro do usuário", () => {
                 expect(res.body.email).toEqual(email);
 
                 return request.post("/user")
-                .send(user)
-                .then(res => {
-                    expect(res.statusCode).toEqual(400);
-                    expect(res.body.error).toEqual("E-mail já cadastrado");
-                }).catch(err => {
-                    expect(err).toMatch('error');
-                });
+                    .send(user)
+                    .then(res => {
+                        expect(res.statusCode).toEqual(400);
+                        expect(res.body.error).toEqual("E-mail já cadastrado");
+                    }).catch(err => {
+                        expect(err).toMatch('error');
+                    });
+            })
+            .catch(err => {
+                expect(err).toMatch('error');
+            });
+    });
+});
+
+describe("Autenticação", () => {
+    test("Deve retornar um token quando logar", () => {
+        return request
+            .post("/auth")
+            .send({ email: mainUser.email, password: mainUser.password })
+            .then(res => {
+                expect(res.statusCode).toEqual(200);
+                expect(res.body.token).toBeDefined();
+            })
+            .catch(err => {
+                expect(err).toMatch('error');
+            });
+    });
+
+    test("Deve impedir que um usuário não cadastrado se logue", () => {
+        return request
+            .post("/auth")
+            .send({ email: "emailnaocadastrado@email.com", password: "123456gfghj" })
+            .then(res => {
+                expect(res.statusCode).toEqual(403);
+                expect(res.body.errors.email).toEqual("E-mail não cadastrado!");
+            })
+            .catch(err => {
+                expect(err).toMatch('error');
+            });
+    });
+
+    
+    test("Deve impedir que um usuário se logue com uma senha errada", () => {
+        return request
+            .post("/auth")
+            .send({ email: mainUser.email, password: "senha-errada" })
+            .then(res => {
+                expect(res.statusCode).toEqual(403);
+                expect(res.body.errors.password).toEqual("Senha inválida!");
             })
             .catch(err => {
                 expect(err).toMatch('error');
